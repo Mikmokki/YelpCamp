f89cbfc0b5b4e4cbc01583c307829e7351888ee6
Mikko Juusti <mikko.juusti@aalto.fi>
2021-01-12 22:58:45 +0200

    improved styling

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const path = require("path")
 const mongoose = require('mongoose');
+const ejsMate = require("ejs-mate")
 const Campground = require("./models/campground")
 const methodOverride = require("method-override")
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -16,7 +17,7 @@ db.once("open", () => {
 })
 
 const app = express();
-
+app.engine("ejs", ejsMate)
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
