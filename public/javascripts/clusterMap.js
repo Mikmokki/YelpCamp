037db2bcfec2d052c7126a9f9b8ff02d05f9ae29
Mikko Juusti <mikko.juusti@aalto.fi>
2021-01-28 10:37:15 +0200

    centered map

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -2,7 +2,7 @@ mapboxgl.accessToken = mapToken
 const map = new mapboxgl.Map({
     container: 'cluster-map',
     style: 'mapbox://styles/mapbox/light-v10',
-    center: [-103.59179687498357, 40.66995747013945],
+    center: [24.835043,60.187246],
     zoom: 3
 });
 
